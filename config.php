c638199911a1756fd0d6672acd66d1b6cd2d23bd
Izbunia <vaniajernovei3@gmail.com>
2024-04-25 07:13:13 +0100

    2

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('APP_NAME', 'Loja PHP');
 define('APP_VERSION','V1.0.0');
 
-// MYSQL - constantes para ligacao BD
+// MYSQL - constantes para ligacao BD de dados
 define('MYSQL_SERVER', 'localhost');
 define('MYSQL_DATABASE', 'php_store');
 define('MYSQL_USER', 'root');
